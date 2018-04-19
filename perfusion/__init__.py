23dee3eb9eb2606eddc773ee102dbbd5b2c140f8
Satyam Ghodasara <sghodas@gmail.com>
2018-04-19 09:42:33 -0400

    Added docstring and dictionary module to __init__.py

--- perfusion/__init__.py
+++ perfusion/__init__.py
@@ -1,2 +1,6 @@
+"""This package performs perfusion modeling using a dictionary matching\
+ approach for DCE-MRI data."""
+
 from perfusion.models import *
 from perfusion.signal_contrast import *
+from perfusion.dictionary import *
