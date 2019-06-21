75a08b5961b996098842a6c905654594d019c55f
Satyam Ghodasara <sghodas@gmail.com>
2019-06-20 23:38:55 -0400

    Update data extraction

--- perfusion/signal_contrast.py
+++ perfusion/signal_contrast.py
@@ -21,17 +21,17 @@ def signal_to_contrast(signal, acquisition_data):
 
 def art_signal_to_contrast(acquisition_data):
     """Convert arterial signal intensity to contrast concentration."""
-    allts = np.asscalar(acquisition_data["allts"])
+    allts = acquisition_data["allts"]
     art_signal = np.abs(allts[:, 1])
     pv_signal = np.abs(allts[:, 2])
-    alpha = np.asscalar(np.asscalar(acquisition_data["flipAngle"])) * np.pi / 180
-    t_10_a = np.asscalar(np.asscalar(acquisition_data["T10b"])) * 1000
-    tr = np.asscalar(np.asscalar(acquisition_data["TR"]))
-    relaxivity = np.asscalar(np.asscalar(acquisition_data["relaxivity"]))
+    alpha = acquisition_data["flipAngle"] * np.pi / 180
+    t_10_a = acquisition_data["T10b"] * 1000
+    tr = acquisition_data["TR"]
+    relaxivity = acquisition_data["relaxivity"]
     hematocrit = 0.4
 
-    start_frame = np.asscalar(np.asscalar(acquisition_data["startFrame"])) - 1
-    add_frames = np.asscalar(np.asscalar(acquisition_data["addFrames"]))
+    start_frame = int(acquisition_data["startFrame"]) - 1
+    add_frames = int(acquisition_data["addFrames"])
     stop_frame = start_frame + add_frames + 1
     m_0 = np.mean(pv_signal[start_frame:stop_frame])
 
@@ -41,16 +41,16 @@ def art_signal_to_contrast(acquisition_data):
 
 def pv_signal_to_contrast(acquisition_data):
     """Convert portal venous signal intensity to contrast concentration."""
-    allts = np.asscalar(acquisition_data["allts"])
+    allts = acquisition_data["allts"]
     pv_signal = np.abs(allts[:, 2])
-    alpha = np.asscalar(np.asscalar(acquisition_data["flipAngle"])) * np.pi / 180
-    t_10_pv = np.asscalar(np.asscalar(acquisition_data["T10p"])) * 1000
-    tr = np.asscalar(np.asscalar(acquisition_data["TR"]))
-    relaxivity = np.asscalar(np.asscalar(acquisition_data["relaxivity"]))
+    alpha = acquisition_data["flipAngle"] * np.pi / 180
+    t_10_pv = acquisition_data["T10p"] * 1000
+    tr = acquisition_data["TR"]
+    relaxivity = acquisition_data["relaxivity"]
     hematocrit = 0.4
 
-    start_frame = np.asscalar(np.asscalar(acquisition_data["startFrame"])) - 1
-    add_frames = np.asscalar(np.asscalar(acquisition_data["addFrames"]))
+    start_frame = int(acquisition_data["startFrame"]) - 1
+    add_frames = int(acquisition_data["addFrames"])
     stop_frame = start_frame + add_frames + 1
     m_0 = np.mean(pv_signal[start_frame:stop_frame])
 
